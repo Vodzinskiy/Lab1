e74a7893f1db2321914b594f09def2842517fb59
Arsen0109 <balasarseniy777@gmail.com>
2022-09-17 19:28:53 +0300

    added beans

--- src/main/java/com/example/lab1/Lab1Application.java
+++ src/main/java/com/example/lab1/Lab1Application.java
@@ -3,8 +3,10 @@ package com.example.lab1;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
+@Order(2)
 public class Lab1Application implements CommandLineRunner {
 
     public static void main(String[] args) {
