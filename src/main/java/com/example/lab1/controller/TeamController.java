222b4bca217e2d7b9cba5a8851bedff6c3e38441
Arsen0109 <balasarseniy777@gmail.com>
2022-09-19 18:00:36 +0300

    update html files

--- src/main/java/com/example/lab1/controller/TeamController.java
+++ src/main/java/com/example/lab1/controller/TeamController.java
@@ -8,8 +8,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TeamController {
     @GetMapping("/team")
-
     public String team(){
         return "index";
+    }
+
+    @GetMapping("/team/dima")
+    public String dima(){
+        return "dmytroInfo";
+    }
+
+    @GetMapping("/team/roma")
+    public String roma(){
+        return "romanInfo";
+    }
+
+    @GetMapping("/team/arsen")
+    public String arsen(){
+        return "arsenInfo";
     }
 }
